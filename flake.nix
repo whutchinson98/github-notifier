02ef29bb4173ab18670e40f5da1edf3102116a89
hutch <will@thehutchery.com>
2025-08-20 15:40:17 -0400

    update flake

--- flake.nix
+++ flake.nix
@@ -42,6 +42,32 @@
         ];
       in
       {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "github-notifier";
+          version = "1.0.0";
+          src = ./.;
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+          ];
+
+          buildInputs = with pkgs; [
+            openssl
+          ];
+
+          meta = with pkgs.lib; {
+            description = "A CLI tool to notify github of new commits";
+            homepage = "https://github.com/whutchinson98/github-notifier";
+            license = licenses.mit;
+            maintainers = [ ];
+            mainProgram = "github-notifier";
+          };
+        };
+
         devShell = pkgs.mkShell {
           buildInputs = packages ++ libraries;
           LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath libraries}";
